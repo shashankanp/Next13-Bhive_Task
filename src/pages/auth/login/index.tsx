7b9037903bb9903a14be1624cb05d8bc6456f2b9
shashankanp <shashanknp02@gmail.com>
2023-05-29 14:51:00 +0530

    Microsoft login working

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -3,7 +3,11 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { FaMicrosoft } from "react-icons/fa";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  OAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 import { auth } from "../../../../utils/firebase";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
@@ -12,18 +16,39 @@ import axios from "axios";
 
 export default function Login() {
   const [user, loading] = useAuthState(auth);
+  const route = useRouter();
 
   // Sign in with Google
-  const route = useRouter();
   const googleProvider = new GoogleAuthProvider();
-  console.log(user?.displayName, user?.email, user?.uid);
+  // console.log(user?.displayName, user?.email, user?.uid);
   const GoogleLogin = async () => {
     try {
       const result = await signInWithPopup(auth, googleProvider).then();
-      // console.log(result.user);
-      if (loading) return <h1 className="text-3xl">Loading...</h1>;
+      console.log(result.user);
       route.push("/dashboard");
-    } catch (error) {
+    } catch (error: any) {
+      console.log(error);
+    }
+  };
+
+  // Sign in with Microsoft
+  const microProvider = new OAuthProvider("microsoft.com");
+  microProvider.setCustomParameters({
+    prompt: "consent",
+    tenant: "696e5350-ad97-4434-9554-59207481a1ed",
+  });
+  const MicrosoftLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, microProvider).then(
+        (result) => {
+          const credential = OAuthProvider.credentialFromResult(result);
+          const accessToken = credential?.accessToken;
+          const idToken = credential?.idToken;
+        }
+      );
+      // console.log(result.user);
+      route.push("/dashboard");
+    } catch (error: any) {
       console.log(error);
     }
   };
@@ -38,12 +63,12 @@ export default function Login() {
         })
         .then((response) => {
           console.log("Firebase User Success:", response);
+          route.push("/dashboard");
           return response.data;
         })
         .catch((err) => console.log("Firebase User Error:", err));
-      route.push("/dashboard");
     } else {
-      console.log("Login");
+      // console.log("Login Please");
     }
   }, [user]);
 
@@ -61,7 +86,10 @@ export default function Login() {
           <FcGoogle className="text-2xl" />
           Sign in with Google
         </button>
-        <button className="text-white bg-gray-700 p-4 w-full font-medium rounded-lg flex align-middle gap-2 ">
+        <button
+          onClick={MicrosoftLogin}
+          className="text-white bg-gray-700 p-4 w-full font-medium rounded-lg flex align-middle gap-2 "
+        >
           <FaMicrosoft className="text-2xl text-blue-500" />
           Sign in with Microsoft
         </button>
