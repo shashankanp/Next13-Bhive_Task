e60b25d0a378efb7cdb347b53424b579e6d97013
shashankanp <shashanknp02@gmail.com>
2023-06-01 13:03:35 +0530

    APIV2.1

--- src/pages/api/storage/post.ts
+++ src/pages/api/storage/post.ts
@@ -6,12 +6,11 @@ const post = async (req: any, res: any) => {
   const { method } = req;
   switch (method) {
     case "POST":
-      const file = req.body;
-      const bucket = admin.storage().bucket();
-      const id = uuidv4(); // Generate unique id for the filename
-      const fileRef = bucket.file(`images/${id}.jpeg`);
-
       try {
+        const file = req.body;
+        const bucket = admin.storage().bucket();
+        const id = uuidv4(); // Generate unique id for the filename
+        const fileRef = bucket.file(`images/${id}.jpeg`);
         await fileRef.save(file, {
           metadata: {
             contentType: "image/jpeg",
