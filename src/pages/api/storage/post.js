bbda7a32a317f21d1ef59462e0ab179d58d769ce
shashankanp <shashanknp02@gmail.com>
2023-06-01 14:30:30 +0530

    API V3.4

--- src/pages/api/storage/post.js
+++ src/pages/api/storage/post.js
@@ -3,6 +3,7 @@ import {
   ref,
   uploadBytesResumable,
   getDownloadURL,
+  uploadBytes,
 } from "firebase/storage";
 import { initializeApp } from "firebase/app";
 import { v4 as uuidv4 } from "uuid";
@@ -20,31 +21,16 @@ const post = async (req: any, res: any) => {
           throw new Error("Invalid request: file and contentType are required");
         }
 
-        const extension = contentType.split("/")[1]; // Extract the file extension from the content type
+        const extension = contentType.split("/")[1];
         const fileBuffer = Buffer.from(base64String, "base64");
         const id = uuidv4();
         const storageRef = ref(storage, `images/${id}.${extension}`);
 
-        const uploadTask = uploadBytesResumable(storageRef, fileBuffer, {
-          contentType,
-        });
+        await uploadBytes(storageRef, fileBuffer);
 
-        uploadTask.on(
-          "state_changed",
-          (snapshot) => {
-            const progress =
-              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log("Upload is " + progress + "% done");
-          },
-          (error) => {
-            console.log("Failed: ", error);
-            res.status(400).json({ success: false, error: error });
-          },
-          async () => {
-            const downloadURL = await getDownloadURL(storageRef);
-            res.status(200).json({ url: downloadURL });
-          }
-        );
+        const downloadURL = await storageRef.getDownloadURL();
+
+        res.status(200).json({ url: downloadURL });
       } catch (error) {
         console.log("Failed: ", error);
         res.status(400).json({ success: false, error: error });
