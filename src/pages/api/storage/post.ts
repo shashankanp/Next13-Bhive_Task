5e2c11066129a3e70096170a18d8a9c04f081b6b
shashankanp <shashanknp02@gmail.com>
2023-06-01 14:03:36 +0530

    API V3.3

--- src/pages/api/storage/post.ts
+++ src/pages/api/storage/post.ts
@@ -1,4 +1,9 @@
-import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
 import { initializeApp } from "firebase/app";
 import { v4 as uuidv4 } from "uuid";
 import { storage } from "../../../../utils/firebase";
@@ -9,20 +14,45 @@ const post = async (req: any, res: any) => {
   switch (method) {
     case "POST":
       try {
-        const fileBuffer = Buffer.from(req.body, "binary"); // Convert the binary data to a Buffer
-        const id = uuidv4(); // Generate unique id for the filename
-        const storageRef = ref(storage, `images/${id}.jpeg`);
-        await uploadBytes(storageRef, fileBuffer);
+        const { file: base64String, contentType } = req.body;
 
-        // Note: getDownloadURL only gets a public URL for the file, which is different from the signed URL
-        // and does not require any authentication to access
-        const downloadUrl = await getDownloadURL(storageRef);
+        if (!base64String || !contentType) {
+          throw new Error("Invalid request: file and contentType are required");
+        }
 
-        res.status(200).json({ url: downloadUrl });
+        const extension = contentType.split("/")[1]; // Extract the file extension from the content type
+        const fileBuffer = Buffer.from(base64String, "base64");
+        const id = uuidv4();
+        const storageRef = ref(storage, `images/${id}.${extension}`);
+
+        const uploadTask = uploadBytesResumable(storageRef, fileBuffer, {
+          contentType,
+        });
+
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+          },
+          (error) => {
+            console.log("Failed: ", error);
+            res.status(400).json({ success: false, error: error });
+          },
+          async () => {
+            const downloadURL = await getDownloadURL(storageRef);
+            res.status(200).json({ url: downloadURL });
+          }
+        );
       } catch (error) {
         console.log("Failed: ", error);
         res.status(400).json({ success: false, error: error });
       }
+      break;
+    default:
+      res.setHeader("Allow", ["POST"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
   }
 };
 export default post;
