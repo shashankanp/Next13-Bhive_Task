fae76951b05f911d6ee90beea12d3b3dfc01904a
shashankanp <shashanknp02@gmail.com>
2023-06-02 12:48:41 +0530

    I think everthing done for API?

--- src/pages/api/storage/post.js
+++ src/pages/api/storage/post.js
@@ -60,15 +60,6 @@ const post = async (req, res) => {
         }
         break;
 
-      case "GET":
-        try {
-          const users = await User.findOne({ firebase_name: name });
-          return res.json({ pic_url: users.display_pic });
-        } catch (error) {
-          res.status(400).json({ success: false, error: error });
-        }
-        break;
-
       default:
         // res.setHeader("Allow", ["POST"]);
         res.status(405).end(`Method ${method} Not Allowed`);
